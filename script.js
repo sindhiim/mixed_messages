25530eea94acf67f9b0a689ae2655d86ba6aa513
SanjayK <sensation117@gmail.com>
2021-03-03 21:20:41 +0530

    how about this way

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-const wordSet_1 = []
-const wordSet_2 = []
-const wordSet_3 = []
+const wordSet_1 = ['My', 'We', 'Their', 'Mohan', 'Sohan\'s', 'Rohan\'s']
+const randIdx1 = Math.floor(Math.random()*wordSet_1.length)
+const wordSet_2 = ['lion', 'camel', 'horse', 'fish']
+const randIdx2 = Math.floor(Math.random()*wordSet_2.length)
+const wordSet_3 = ['house', 'tree', 'cupboard', 'box']
+const randIdx3 = Math.floor(Math.random()*wordSet_3.length)
 const wordSet_4 = []
 const wordSet_5 = []
 const wordSet_6 = []
 
-const randIdx = Math.floor(Math.random()*5)
-console.log(wordSet_1[randIdx] +' '+ wordSet_2[randIdx]+' '+ wordSet_3[randIdx]+' '+wordSet_4[randIdx]) 
+
+console.log(`${wordSet_1[randIdx1]} favourite animal is ${wordSet_2[randIdx2]}. ${wordSet_1[randIdx1]} live in a ${wordSet_3[randIdx3]}`);
